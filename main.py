d62566ea8c3b14b8bac38cd71e3eb172e4d0f1cc
Alihan Karatatar <alihankaratatar@gmail.com>
2022-04-13 19:16:41 +0300

    load fonskiyonu yuklendi

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F # import convolution functions like Relu
 import torch.optim as optim # optimzer
 
 train_path = "./pa3_dataset/"
+test_path= "./Vegetable/test"
 transform = transforms.Compose( # composing several transforms together
     [transforms.ToTensor(), # to tensor object
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # mean = 0.5, std = 0.5
@@ -18,6 +19,14 @@ batch_size = 5
 
 num_workers = 2
 
-train_set = torchvision.datasets.ImageFolder(root = train_path, transform=transform)
+# Train Data Load
 
-print(train_set)
+train_set = torchvision.datasets.ImageFolder(root = train_path, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                          shuffle=True, num_workers=num_workers)
+
+# Test Data Load
+
+test_set = torchvision.datasets.ImageFolder(root = test_path, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                          shuffle=True, num_workers=num_workers)
