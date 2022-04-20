2ad8b0c00231e9c130da290378786d39ef48ea0d
Alihan Karatatar <alihankaratatar@gmail.com>
2022-04-21 00:30:20 +0300

    "model builded"

--- main.py
+++ main.py
@@ -1,124 +1,88 @@
 import torch
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-from torch.optim import lr_scheduler
-from torch import optim
-from torchvision.datasets import ImageFolder
-from torchvision.utils import make_grid
-from torch.utils.data import Dataset, DataLoader
-import matplotlib.pyplot as plt
-from torchvision import models
-from torchvision import transforms
-from tqdm import tqdm
+import torch.optim as optim
+import os
 
+batch = 5
+workers = 1
+epochs = 2 
+transformed = transforms.Compose([transforms.ToTensor(),
+                                transforms.Resize((64, 64)),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+train_set = datasets.ImageFolder(root='./Vegetable/train/', transform = transformed)
+test_set = datasets.ImageFolder(root='./Vegetable/test/',transform = transformed )  
 
+train_loader = torch.utils.data.DataLoader(train_set, batch, shuffle = True,  num_workers = workers)
+test_loader = torch.utils.data.DataLoader(test_set, batch, shuffle = True, num_workers = workers)
 
-path = './Vegetable/'
-
-# create dataloaders
-tfms = transforms.Compose([transforms.Resize((512,512)), 
-                           transforms.ToTensor(),
-                           transforms.Normalize([0.458, 0.456, 0.406],
-                                                [0.229, 0.224, 0.225])])
-
-train = ImageFolder('Vegetable/train/', tfms)
-valid = ImageFolder('Vegetable/validation/', tfms)
-
-
-train_data_loader = DataLoader(train, batch_size=64, num_workers=4)
-valid_data_loader = DataLoader(valid, batch_size = 64, num_workers = 4)
+classes = os.listdir("Vegetable/train")
 
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ConvLayer1 = nn.Sequential(
-            nn.Conv2d(3, 8, 3), # inp (3, 512, 512)
-            nn.Conv2d(8, 16, 3),
-            nn.MaxPool2d(2),
-            nn.ReLU() # op (16, 256, 256)
-        )
-        self.ConvLayer2 = nn.Sequential(
-            nn.Conv2d(16, 32, 5), # inp (16, 256, 256)
-            nn.Conv2d(32, 32, 3),
-            nn.MaxPool2d(4),
-            nn.ReLU() # op (32, 64, 64)
-        )
-        self.ConvLayer3 = nn.Sequential(
-            nn.Conv2d(32, 64, 3), # inp (32, 64, 64)
-            nn.Conv2d(64, 64, 5),
-            nn.MaxPool2d(2),
-            nn.ReLU() # op (64, 32, 32)
-        )
-        self.ConvLayer4 = nn.Sequential(
-            nn.Conv2d(64, 128, 5), # inp (64, 32, 32)
-            nn.Conv2d(128, 128, 3),
-            nn.MaxPool2d(2),
-            nn.ReLU() # op (128, 16, 16)
-        )
-        self.Lin1 = nn.Linear(15488, 15)
-        self.Lin2 = nn.Linear(1500, 150)
-        self.Lin3 = nn.Linear(150, 15)
-        
-        
+        self.conv1 = nn.Conv2d(3, 6, 3, 1, 1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 10, 3, 1, 1)
+        self.fc1 = nn.Linear(640, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 15)
+    
     def forward(self, x):
-        x = self.ConvLayer1(x)
-        x = self.ConvLayer2(x)
-        x = self.ConvLayer3(x)
-        x = self.ConvLayer4(x)
-        x = x.view(x.size(0), -1)
-        x = self.Lin1(x)
-       
-        
-        return F.log_softmax(x, dim = 1)
-
-model = Net()
-# model = torch.load('800ephsapparel')
-# model = model.train()
-
-
-#optimizer = optim.SGD(model.parameters(), lr = 0.01, momentum = 0.5)
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 def main():
+    net = Net()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.5)
-    for epoch in tqdm(range(50)):  # loop over the dataset multiple times
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(5):  # loop over the dataset multiple times
 
         running_loss = 0.0
-        for i, data in enumerate(train_data_loader, 0):
+        for i, data in enumerate(train_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
-
             inputs, labels = data
+
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = model(inputs)
+            outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-           # if i % 10 == 0:    # print every 10 mini-batches
-            #    print('[%d, %5d] loss: %.3f' %
-             #         (epoch + 1, i + 1, running_loss / 2000))
-              #  running_loss = 0.0
+            if i % 250 == 249:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 250:.3f}')
+                running_loss = 0.0
 
     print('Finished Training')
 
     correct = 0
     total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
-        for data in valid_data_loader:
+        for data in test_loader:
             images, labels = data
-            outputs = model(images)
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the test images: %d %%' % (
-        100 * correct / total))
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+        main()
